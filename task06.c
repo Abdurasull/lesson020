9dada50ada6e4e7be35e83e58f76d1f3619d19e8
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-13 06:45:40 +0500

    task06.c bajarildi

---
+++ task06.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int order_line(int a, int b)
+{//ikki sonni ikubini topuvchi recurcive function
+    if (b != 0)
+        return gcd(b, a % b);
+        else
+        {
+            return a;
+        }
+        
+    
+}
+
+int main()
+{
+    int n1, n2;
+
+    printf("1 - sonni kiriting: ");
+    scanf("%d", &n1);
+    printf("Ikki sonni kiriting: ");
+    scanf("%d", &n1);
+
+    printf(" EKUBi: %d\n", order_line(n1, n2));
+
+    return 0;
+}
