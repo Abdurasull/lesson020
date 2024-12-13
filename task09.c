5455f31620f6ddbc6c88e2e87a2f9b6dcbbe212e
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-13 09:32:23 +0500

    task09.c bajarildi

---
+++ task09.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int pow_function(int n, int m){
+    // n ni m chi darajasini hisoblovchi function
+    if (m == 0) return 1;
+
+    return n * pow_function(n, m - 1);
+}
+int main(){
+    int a, b;
+    printf("1-sonni kiriting: ");
+    scanf("%d", &a);
+
+    printf("2-sonni kiriting: ");
+    scanf("%d", &b);
+
+    printf("Result: %d", pow_function(a, b));
+}
