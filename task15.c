fa4f1ea08ae34761a6011507b2c1f1e93fdbc9c3
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-13 10:22:39 +0500

    task15.c bajarildi

---
+++ task15.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int i = 0;
+
+void func(char s[])
+{
+
+    int length = strlen(s);
+
+    if (i - length == 0)
+        return;
+
+    putchar(s[length - i - 1]);
+
+    i++;
+
+    func(s);
+}
+
+int main()
+{
+
+    func("Assalomu alaykum");
+
+    return 0;
+}
